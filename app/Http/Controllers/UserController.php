f6e71855e60ee431d910f75ed46185ea9fa7ae12
hamaa-affix <hamaagive1108@gmail.com>
2020-04-18 11:33:11 +0000

    usercontroller内のedit,update処理の追加、ルーテングの設定

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use Intervention\Image\Facades\Image; 
+
+use App\Services\CheckExtensionServices; 
+use App\Services\FileUploadServices; 
 
 use Illuminate\Http\Request;
 use App\User;
@@ -13,5 +17,43 @@ class UserController extends Controller
         //findorFailでidに紐ずくカラムを取得、なければ例外処理を行う。
         $user = User::findorFail($id);
         return view('users.show', compact('user'));
+    }
+    
+    //編集画面の処理
+    public function edit($id)
+    {
+        $user = User::findorFail($id);
+
+        return view('users.edit', compact('user')); 
+    }
+    
+    //データの更新処理
+    public function update(Request $request, $id)
+    {
+
+        $user = User::findorFail($id);
+        
+        //画像ファイルがアップロードされているかどうか判定。
+        if(!is_null($request['img_name'])){
+            $imageFile = $request['img_name'];
+
+            $list = FileUploadServices::fileUpload($imageFile);
+            list($extension, $fileNameToStore, $fileData) = $list;
+            
+            $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
+            $image = Image::make($data_url);        
+            $image->resize(400,400)->save(storage_path() . '/app/public/images/' . $fileNameToStore );
+
+            $user->img_name = $fileNameToStore;
+        }
+        
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->sex = $request->sex;
+        $user->self_introduction = $request->self_introduction;
+
+        $user->save();
+
+        return redirect('home'); 
     }
 }
